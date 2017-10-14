f2e43ccc5e2ac345ecba2fab709c5018c009f6d7
Kristian Gali <kristiangali@gmail.com>
2017-10-14 16:30:02 -0600

    Added more classes to config

--- config.php
+++ config.php
@@ -156,6 +156,311 @@ class Tue {
 			}
 		}
 }
+class Wed {
+
+		public function create($event, $time)
+		{
+			$date = date("Y-m-d H:i:s");
+			$modified = date("Y-m-d H:i:s");
+			$status = 'awaiting';
+			if(!empty($event) && !empty($time))
+			{
+				$create = Wednesday::create(array('today' => $date, 'time' => $time, 'event' => $event, 'status' => $status));
+				if(!$create)
+				{
+					$message = 'Event or Time had an error or was empty';
+
+				} 
+
+				$message = 'Event for '.$time.' was created';
+
+			} else {
+				$message = 'Please enter the event & time';
+			}
+			return $message;
+		}
+		public function update($event_id, $event, $newtime)
+		{
+
+			$update->modified = date("Y-m-d H:i:s");
+			$update->event = $event;
+			$update->time = $newtime;
+			$update->save();
+		}
+		public function deleteSingleEvent($event_id)
+		{
+				$doDelete = Wednesday::find_by_id($event_id);
+				$doDelete->delete();
+				if($doDelete)
+				{
+					$message = "deleted";
+				} else {
+					$message = "event not found.";
+				}
+				return $message;
+		}
+		public function getAllMonEvents()
+		{
+			$all = Wednesday::all();
+			foreach($all as $events)
+			{
+				return $event = $events->event;
+			}
+
+		}
+		public function getSingleMonEvent($event_id)
+		{
+			$doFind = Wednesday::find_by_id($event_id);
+			foreach($doFind as $found)
+			{
+				return $foundEvent = $found->event;
+			}
+		}
+}
+class Thu {
+
+		public function create($event, $time)
+		{
+			$date = date("Y-m-d H:i:s");
+			$modified = date("Y-m-d H:i:s");
+			$status = 'awaiting';
+			if(!empty($event) && !empty($time))
+			{
+				$create = Thursday::create(array('today' => $date, 'time' => $time, 'event' => $event, 'status' => $status));
+				if(!$create)
+				{
+					$message = 'Event or Time had an error or was empty';
+
+				} 
+
+				$message = 'Event for '.$time.' was created';
+
+			} else {
+				$message = 'Please enter the event & time';
+			}
+			return $message;
+		}
+		public function update($event_id, $event, $newtime)
+		{
+
+			$update->modified = date("Y-m-d H:i:s");
+			$update->event = $event;
+			$update->time = $newtime;
+			$update->save();
+		}
+		public function deleteSingleEvent($event_id)
+		{
+				$doDelete = Thursday::find_by_id($event_id);
+				$doDelete->delete();
+				if($doDelete)
+				{
+					$message = "deleted";
+				} else {
+					$message = "event not found.";
+				}
+				return $message;
+		}
+		public function getAllMonEvents()
+		{
+			$all = Thursday::all();
+			foreach($all as $events)
+			{
+				return $event = $events->event;
+			}
+
+		}
+		public function getSingleMonEvent($event_id)
+		{
+			$doFind = Thursday::find_by_id($event_id);
+			foreach($doFind as $found)
+			{
+				return $foundEvent = $found->event;
+			}
+		}
+}
+class Fri {
+
+		public function create($event, $time)
+		{
+			$date = date("Y-m-d H:i:s");
+			$modified = date("Y-m-d H:i:s");
+			$status = 'awaiting';
+			if(!empty($event) && !empty($time))
+			{
+				$create = Friday::create(array('today' => $date, 'time' => $time, 'event' => $event, 'status' => $status));
+				if(!$create)
+				{
+					$message = 'Event or Time had an error or was empty';
+
+				} 
+
+				$message = 'Event for '.$time.' was created';
+
+			} else {
+				$message = 'Please enter the event & time';
+			}
+			return $message;
+		}
+		public function update($event_id, $event, $newtime)
+		{
+
+			$update->modified = date("Y-m-d H:i:s");
+			$update->event = $event;
+			$update->time = $newtime;
+			$update->save();
+		}
+		public function deleteSingleEvent($event_id)
+		{
+				$doDelete = Friday::find_by_id($event_id);
+				$doDelete->delete();
+				if($doDelete)
+				{
+					$message = "deleted";
+				} else {
+					$message = "event not found.";
+				}
+				return $message;
+		}
+		public function getAllMonEvents()
+		{
+			$all = Friday::all();
+			foreach($all as $events)
+			{
+				return $event = $events->event;
+			}
+
+		}
+		public function getSingleMonEvent($event_id)
+		{
+			$doFind = Friday::find_by_id($event_id);
+			foreach($doFind as $found)
+			{
+				return $foundEvent = $found->event;
+			}
+		}
+}
+class Sat {
+
+		public function create($event, $time)
+		{
+			$date = date("Y-m-d H:i:s");
+			$modified = date("Y-m-d H:i:s");
+			$status = 'awaiting';
+			if(!empty($event) && !empty($time))
+			{
+				$create = Saturday::create(array('today' => $date, 'time' => $time, 'event' => $event, 'status' => $status));
+				if(!$create)
+				{
+					$message = 'Event or Time had an error or was empty';
+
+				} 
+
+				$message = 'Event for '.$time.' was created';
+
+			} else {
+				$message = 'Please enter the event & time';
+			}
+			return $message;
+		}
+		public function update($event_id, $event, $newtime)
+		{
+
+			$update->modified = date("Y-m-d H:i:s");
+			$update->event = $event;
+			$update->time = $newtime;
+			$update->save();
+		}
+		public function deleteSingleEvent($event_id)
+		{
+				$doDelete = Saturday::find_by_id($event_id);
+				$doDelete->delete();
+				if($doDelete)
+				{
+					$message = "deleted";
+				} else {
+					$message = "event not found.";
+				}
+				return $message;
+		}
+		public function getAllMonEvents()
+		{
+			$all = Saturday::all();
+			foreach($all as $events)
+			{
+				return $event = $events->event;
+			}
+
+		}
+		public function getSingleMonEvent($event_id)
+		{
+			$doFind = Saturday::find_by_id($event_id);
+			foreach($doFind as $found)
+			{
+				return $foundEvent = $found->event;
+			}
+		}
+}
+class Sun {
+
+		public function create($event, $time)
+		{
+			$date = date("Y-m-d H:i:s");
+			$modified = date("Y-m-d H:i:s");
+			$status = 'awaiting';
+			if(!empty($event) && !empty($time))
+			{
+				$create = Sunday::create(array('today' => $date, 'time' => $time, 'event' => $event, 'status' => $status));
+				if(!$create)
+				{
+					$message = 'Event or Time had an error or was empty';
+
+				} 
+
+				$message = 'Event for '.$time.' was created';
+
+			} else {
+				$message = 'Please enter the event & time';
+			}
+			return $message;
+		}
+		public function update($event_id, $event, $newtime)
+		{
+
+			$update->modified = date("Y-m-d H:i:s");
+			$update->event = $event;
+			$update->time = $newtime;
+			$update->save();
+		}
+		public function deleteSingleEvent($event_id)
+		{
+				$doDelete = Sunday::find_by_id($event_id);
+				$doDelete->delete();
+				if($doDelete)
+				{
+					$message = "deleted";
+				} else {
+					$message = "event not found.";
+				}
+				return $message;
+		}
+		public function getAllMonEvents()
+		{
+			$all = Sunday::all();
+			foreach($all as $events)
+			{
+				return $event = $events->event;
+			}
+
+		}
+		public function getSingleMonEvent($event_id)
+		{
+			$doFind = Sunday::find_by_id($event_id);
+			foreach($doFind as $found)
+			{
+				return $foundEvent = $found->event;
+			}
+		}
+}
 // $test = new Mon(); 
 // $test->create('Kristian Rocks.', 'four_fourteen');
 // $test->deleteSingleEvent(3);
